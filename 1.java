726e006b1f93851465a45ad49f923d61713512d5
omkar200425 <mathapatiom1@gmail.com>
2025-04-04 08:33:56 +0530

    save

---
+++ 1.java
@@ -0,0 +1,16 @@
+class Main {
+  private String name;
+
+  // constructor
+  Main() {
+    System.out.println("Constructor Called:");
+    name = "Programiz";
+  }
+
+  public static void main(String[] args) {
+
+    
+    Main obj = new Main();
+    System.out.println("The name is " + obj.name);
+  }
+}
